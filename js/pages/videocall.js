e0ba175317aa2dce39656bfcbc123e7a1f7326e2
siamamaga <melahel12@gmail.com>
2026-04-20 13:00:13 +0200

    fix: ajouter serveurs TURN pour WebRTC NAT traversal

--- js/pages/videocall.js
+++ js/pages/videocall.js
@@ -19,10 +19,25 @@ const VideoCall = (() => {
   let pendingOffer = null;
   let pendingFrom = null;
 
-  const ICE_SERVERS = {
+ const ICE_SERVERS = {
     iceServers: [
       { urls: 'stun:stun.l.google.com:19302' },
       { urls: 'stun:stun1.l.google.com:19302' },
+      {
+        urls: 'turn:openrelay.metered.ca:80',
+        username: 'openrelayproject',
+        credential: 'openrelayproject'
+      },
+      {
+        urls: 'turn:openrelay.metered.ca:443',
+        username: 'openrelayproject',
+        credential: 'openrelayproject'
+      },
+      {
+        urls: 'turn:openrelay.metered.ca:443?transport=tcp',
+        username: 'openrelayproject',
+        credential: 'openrelayproject'
+      }
     ]
   };
 
